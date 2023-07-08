2f57b386ff32591ce1c366ecea06fbfb1b7532f7
Abdul Basha <abdul.basha@argano.com>
2023-07-08 11:54:02 +0530

    code updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route('/whoami')
 def hello():
-    return 'Know about me in here: https://www.linkedin.com/in/itsmegaffoor/'
+    return 'Connect with me in here: https://www.linkedin.com/in/itsmegaffoor/'
 
 if __name__ == "main":
     app.run(host="0.0.0.0", port=8080)
